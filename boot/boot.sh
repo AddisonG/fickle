38def58513cef969d5617ec6bed1a36998048413
Addison Gourluck <addison@gourluck.com>
2024-06-03 06:17:43 +0000

    Added boot script

---
+++ boot/boot.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+touch /tmp/fickle
+
+hostname -b "ficklehost"
+
+# Replace the ubuntu user with fickle
+usermod -l fickle -d /home/fickle -m ubuntu
+groupmod -n fickle ubuntu
+usermod -aG www-data fickle
+usermod -aG fastapiuser fickle
+rm -rf /home/ubuntu
+rm /etc/sudoers.d/90-cloud-init-users
+rm -f /etc/update-motd.d/*
+echo "fickle ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+# Create the API user
+adduser --system --group --no-create-home --shell /bin/false fastapiuser
+#usermod -aG www-data fastapiuser
+sudo ln -siT /home/fickle/web/rpg/api/rpg-api /usr/lib/python3/dist-packages/rpg_api
+
+# Install stuff
+apt install -y npm nodejs curl nginx python3-pip jq tree
+apt upgrade
+pip install uvicorn fastapi sqlalchemy
+npm install -g n
+n stable
+npm install -g npm create-react-app
+
+cat >> /etc/fstab <<EOF
+overlay /home/fickle/overlay/merged overlay lowerdir=/etc,upperdir=/home/fickle/overlay/etc,workdir=/home/fickle/overlay/temp 0 0
+/home/fickle/overlay/merged /etc none bind 0 0
+EOF
+
+# "Are you bind mounting an overlay fs over /etc?" - You, probably
+mount -t overlay overlay -o lowerdir=/etc,upperdir=/home/fickle/overlay/etc,workdir=/home/fickle/overlay/temp /home/fickle/overlay/merged
+mount --bind /home/fickle/overlay/merged /etc
+
+service nginx restart
+#systemctl enable dnd-api
+#systemctl start dnd-api
+
+# Create 1GB swapfile
+fallocate -l 1G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
